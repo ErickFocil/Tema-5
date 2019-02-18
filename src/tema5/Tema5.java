5bf388beba1a6f35184854395d380fd4c01e1892
Erick Focil <edfocilg@hotmail.com>
2019-02-18 07:41:12 -0600

    Peticion de parámetros y creacion de funcion

--- src/tema5/Tema5.java
+++ src/tema5/Tema5.java
@@ -1,21 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tema5;
 
-/**
- *
- * @author edfoc
- */
+import java.util.Scanner;
+
 public class Tema5 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Scanner sc = new Scanner(System.in);
+        double ladoA, ladoB, hipotenusa;
+        
+        System.out.println("Ingrese el lado A");
+        ladoA = sc.nextDouble();
+        System.out.println("Ingrese el lado B");
+        ladoB = sc.nextDouble();
+        
+        hipotenusa = CalcularHipotenusa(ladoA,ladoB);
+        
+        System.out.println("Tu hipotenusa mide " + hipotenusa);
+        
+    }
+    
+    public static double CalcularHipotenusa(double ladoA, double ladoB){
+        return Math.sqrt(ladoA*ladoA+ladoB*ladoB);
     }
     
 }
